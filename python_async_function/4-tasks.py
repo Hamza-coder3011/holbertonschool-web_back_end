26cfb8ccccdf7389b9bc2717c2e738cd08643eed
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-11-25 11:41:36 +0100

    last task

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
 This module defines a coroutine that spawns multiple asyncio.Tasks of
-wait_random concurrently and returns their results in
-ascending order of completion.
+wait_random concurrently and returns their results
+in ascending order of completion.
 """
 
 import asyncio
@@ -15,17 +15,12 @@ async def task_wait_n(n: int, max_delay: int) -> list[float]:
     of all delays in ascending order of completion.
 
     Args:
-        n (int): Number of times to spawn task_wait_random.
+        n (int): Number of tasks to spawn.
         max_delay (int): Maximum delay to pass to each task.
 
     Returns:
         list[float]: List of delays in ascending order without using sort().
     """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-    results = []
-
-    for task in asyncio.as_completed(tasks):
-        result = await task
-        results.append(result)
-
-    return results
+    return [await task for task in asyncio.as_completed(
+        [task_wait_random(max_delay) for _ in range(n)]
+    )]
