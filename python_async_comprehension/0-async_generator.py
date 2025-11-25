cb8cac4d1caaade5ab32fded56271714d3ef1636
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-11-25 12:13:15 +0100

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,9 @@
 import asyncio
 import random
 
-
 async def async_generator():
     """Asynchronously generate 10 random numbers between 0 and 10."""
+    random.seed(42)
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
