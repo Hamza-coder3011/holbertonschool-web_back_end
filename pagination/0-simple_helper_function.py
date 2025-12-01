1a84895cc78327dc1e2fd085436892b59ede2e4b
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-12-01 09:27:43 +0100

    task 0

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Function named index_range that takes two integer arguments
+page and page_size.
+The function should return a tuple of size two containing a start index and
+an end index corresponding to the range of indexes to return in a list for
+those particular pagination parameters
+"""
+
+
+def index_range(page: int, page_size: int):
+    """return a tuple of size two containing a start index and
+    an end index corresponding to the range of indexes to return in a
+    list for those particular pagination parameters"""
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
