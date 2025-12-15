a0609e3e5a64058eedcc50a7e8ec0afe5a2069fc
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-12-15 10:52:13 +0100

    task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -6,15 +6,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName)
-  ]).then(results => 
-    results.map(result => {
-      if (result.status === 'fulfilled') {
-        return { status: result.status, value: result.value };
-      } else {
-        // Convert reason to string if it's an Error
-        const reason = result.reason instanceof Error ? result.reason.message : result.reason;
-        return { status: result.status, value: reason };
-      }
-    })
-  );
+  ]);
 }
