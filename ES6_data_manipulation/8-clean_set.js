f5df70d4d113399475292ac7a0640c5369cadc16
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-12-15 10:07:11 +0100

    task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
+  if (typeof startString !== 'string' || startString.length === 0) return '';
   return [...set]
     .filter(value => value.startsWith(startString))
     .map(value => value.slice(startString.length))
